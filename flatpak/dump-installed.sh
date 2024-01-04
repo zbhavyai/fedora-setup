7ab0814306bdba7398ccff9c1b6c40b546f228ef
Bhavyai Gupta <zbhavyai@gmail.com>
2024-01-04 12:38:00 -0700

    update file name

--- flatpak/dump-installed.sh
+++ flatpak/dump-installed.sh
@@ -4,6 +4,6 @@
 # description   : get list of installed flatpak apps
 
 SCRIPT_DIR="$(dirname "$0")"
-OUTPUT_FILE="${SCRIPT_DIR}/flatpak-installed.txt"
+OUTPUT_FILE="${SCRIPT_DIR}/dump-installed.txt"
 
 flatpak list --app --columns=application | sort > "${OUTPUT_FILE}"
