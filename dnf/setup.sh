45269ccd4e7ef7386fb96dd310111ed5585e3641
Bhavyai Gupta <zbhavyai@gmail.com>
2024-01-15 22:10:45 -0700

    setup for packages

---
+++ dnf/setup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# author        : bhavyai
+# description   : install and remove packages
+
+SCRIPT_DIR="$(dirname "$0")"
+FILE_INSTALL="${SCRIPT_DIR}/install.txt"
+FILE_REMOVE="${SCRIPT_DIR}/remove.txt"
+
+install_packages() {
+    packages=()
+    while IFS= read -r line || [[ -n "$line" ]]; do
+        # Skip empty lines and lines starting with #
+        if [[ -z "$line" || "$line" =~ ^#.* ]]; then
+            continue
+        fi
+
+        # Add packages to the array
+        packages+=("$line")
+    done < "${FILE_INSTALL}"
+
+    printf '[INFO] Install: %s\n' "${packages[*]}"
+    sudo dnf install -y "${packages[@]}"
+    printf "\n\n"
+}
+
+remove_packages() {
+    packages=()
+    while IFS= read -r line || [[ -n "$line" ]]; do
+        # Skip empty lines and lines starting with #
+        if [[ -z "$line" || "$line" =~ ^#.* ]]; then
+            continue
+        fi
+
+        # Add packages to the array
+        packages+=("$line")
+    done < "${FILE_REMOVE}"
+
+    printf '[INFO] Remove: %s\n' "${packages[*]}"
+    sudo dnf remove -y "${packages[@]}"
+    printf "\n\n"
+}
+
+
+main() {
+    remove_packages
+    install_packages
+}
+
+main
