50e436f64855cfb6e591afe3b61f6a2161ce2f33
Bhavyai Gupta <zbhavyai@gmail.com>
2024-01-20 12:55:38 -0700

    flatpak output file rename

--- flatpak/dump-installed.sh
+++ flatpak/dump-installed.sh
@@ -4,6 +4,6 @@
 # description   : get list of installed flatpak apps
 
 SCRIPT_DIR="$(dirname "$0")"
-OUTPUT_FILE="${SCRIPT_DIR}/dump-installed.txt"
+OUTPUT_FILE="${SCRIPT_DIR}/install.txt"
 
 flatpak list --app --columns=application | sort > "${OUTPUT_FILE}"
