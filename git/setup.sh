dd67448cbaa3db1b13987aff92832db687c1b604
Bhavyai Gupta <zbhavyai@gmail.com>
2024-01-20 13:04:34 -0700

    update spacing

--- git/setup.sh
+++ git/setup.sh
@@ -10,12 +10,13 @@ CONFIG_FILE_WORK="${SCRIPT_DIR}/.gitconfig-work"
 setup_config() {
     printf '[INFO] Setting up gitconfig: %s\n' "${CONFIG_FILE_HOME}"
     cp -p "${CONFIG_FILE_HOME}" "${HOME}/"
-    
+
     printf '[INFO] Setting up gitconfig: %s\n' "${CONFIG_FILE_WORK}"
     cp -p "${CONFIG_FILE_WORK}" "${HOME}/"
 
-    printf "\n\n"
+    printf "\n"
 }
+
 
 main() {
     setup_config
