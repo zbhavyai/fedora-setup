3a7757915722d578f59fae4a99936cc6ba9512a4
Bhavyai Gupta <zbhavyai@gmail.com>
2024-01-21 13:41:48 -0700

    remove java and jupyter extensions

--- vscode/dump-installed.sh
+++ vscode/dump-installed.sh
@@ -4,6 +4,6 @@
 # description   : get list of installed VS Code extensions
 
 SCRIPT_DIR="$(dirname "$0")"
-OUTPUT_FILE="${SCRIPT_DIR}/dump-installed.txt"
+OUTPUT_FILE="${SCRIPT_DIR}/install.txt"
 
 code --list-extensions | sort > "${OUTPUT_FILE}"
