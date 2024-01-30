2999aba616f7dfd39e136810d3c8c9892b8a21ee
Bhavyai Gupta <zbhavyai@gmail.com>
2024-01-29 18:35:14 -0700

    add vscode installation in setup

--- dnf/setup.sh
+++ dnf/setup.sh
@@ -7,6 +7,12 @@ SCRIPT_DIR="$(dirname "$0")"
 FILE_INSTALL="${SCRIPT_DIR}/install.txt"
 FILE_REMOVE="${SCRIPT_DIR}/remove.txt"
 
+setup_repos() {
+    # vs code: https://code.visualstudio.com/docs/setup/linux#_rhel-fedora-and-centos-based-distributions
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+}
+
 install_packages() {
     packages=()
     while IFS= read -r line || [[ -n "$line" ]]; do
@@ -44,6 +50,7 @@ remove_packages() {
 
 main() {
     remove_packages
+    setup_repos
     install_packages
 }
 
