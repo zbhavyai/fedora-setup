5b52bff3b7da2e8fbea66bd3572381be090c1e8b
Bhavyai Gupta <zbhavyai@gmail.com>
2025-05-23 13:40:29 -0600

    move gnome-text-editor to ansible

--- scripts/dump_gnome_text_editor.sh
+++ scripts/dump_gnome_text_editor.sh
@@ -2,14 +2,12 @@
 # Author        : github.com/zbhavyai
 # Purpose       : Update gnome-text-editor preferences dump
 
-
 SCRIPT_DIR="$(dirname "$0")"
-CONFIG_PREF="${SCRIPT_DIR}/preferences.conf"
+CONFIG_PREF="${SCRIPT_DIR}/../roles/gnome-text-editor/files/preferences.conf"
 
 dump_config() {
-    dconf dump /org/gnome/TextEditor/ > "${CONFIG_PREF}"
+    dconf dump /org/gnome/TextEditor/ >"${CONFIG_PREF}"
 }
-
 
 main() {
     dump_config
