328c9deec71ebe10c307b09f65698e84192d5e38
Bhavyai Gupta <zbhavyai@gmail.com>
2025-05-23 13:53:33 -0600

    move nautilus to ansible

--- scripts/dump_nautilus.sh
+++ scripts/dump_nautilus.sh
@@ -2,14 +2,12 @@
 # Author        : github.com/zbhavyai
 # Purpose       : Update nautilus preferences dump
 
-
 SCRIPT_DIR="$(dirname "$0")"
-CONFIG_PREF="${SCRIPT_DIR}/preferences.conf"
+CONFIG_PREF="${SCRIPT_DIR}/../roles/nautilus/files/preferences.conf"
 
 dump_config() {
-    dconf dump /org/gnome/nautilus/preferences/ > "${CONFIG_PREF}"
+    dconf dump /org/gnome/nautilus/preferences/ >"${CONFIG_PREF}"
 }
-
 
 main() {
     dump_config
