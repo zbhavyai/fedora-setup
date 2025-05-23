25f097ef46c8e599d805113481a76e58b936a59b
Bhavyai Gupta <zbhavyai@gmail.com>
2025-05-23 12:53:09 -0600

    move gnome-terminal to ansible

--- scripts/dump_keybindings.sh
+++ scripts/dump_keybindings.sh
@@ -2,20 +2,16 @@
 # Author        : github.com/zbhavyai
 # Purpose       : Update key bindings configuration dumps
 
-
 SCRIPT_DIR="$(dirname "$0")"
 KB_SCREENSHOT="${SCRIPT_DIR}/../roles/keybindings/files/screenshot.conf"
 KB_MEDIA_KEYS="${SCRIPT_DIR}/../roles/keybindings/files/mediakeys.conf"
 KB_SWITCH_WINDOWS="${SCRIPT_DIR}/../roles/keybindings/files/switchWindows.conf"
-KB_TERMINAL="${SCRIPT_DIR}/../roles/keybindings/files/terminal.conf"
 
 dump_config() {
-    dconf dump /org/gnome/shell/keybindings/ > "${KB_SCREENSHOT}"
-    dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > "${KB_MEDIA_KEYS}"
-    dconf dump /org/gnome/desktop/wm/keybindings/ > "${KB_SWITCH_WINDOWS}"
-    dconf dump /org/gnome/terminal/legacy/keybindings/ > "${KB_TERMINAL}"
+    dconf dump /org/gnome/shell/keybindings/ >"${KB_SCREENSHOT}"
+    dconf dump /org/gnome/settings-daemon/plugins/media-keys/ >"${KB_MEDIA_KEYS}"
+    dconf dump /org/gnome/desktop/wm/keybindings/ >"${KB_SWITCH_WINDOWS}"
 }
-
 
 main() {
     dump_config
