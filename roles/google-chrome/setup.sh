7ba6b9c02a7c6407acdaf365a363583a5e2416a8
Bhavyai Gupta <zbhavyai@gmail.com>
2025-05-23 21:57:28 -0600

    move google chrome as well

--- roles/google-chrome/setup.sh
+++ roles/google-chrome/setup.sh
@@ -2,16 +2,14 @@
 # Author        : github.com/zbhavyai
 # Purpose       : Install Google Chrome
 
-
 SCRIPT_DIR="$(dirname "$0")"
 PKG_FILE="${HOME}/Downloads/google-chrome-stable_current_x86_64.rpm"
 
 install_packages() {
-    wget --quiet  --output-document="${PKG_FILE}" https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+    wget --quiet --output-document="${PKG_FILE}" https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
     sudo rpm --install "${PKG_FILE}"
     rm "${PKG_FILE}"
 }
-
 
 main() {
     install_packages
