543033c561e86c7e3c0116492ec8580f52edd297
Bhavyai Gupta <zbhavyai@gmail.com>
2025-05-24 23:58:32 -0600

    automate app picker and favorites

--- scripts/sync_settings.sh
+++ scripts/sync_settings.sh
@@ -2,15 +2,17 @@
 
 SCRIPT_DIR="$(dirname "$0")"
 CONFIG_APP_FOLDERS="${SCRIPT_DIR}/../roles/settings/files/appFolders.conf"
+CONFIG_APP_PICKER="${SCRIPT_DIR}/../roles/settings/files/appPicker.conf"
 CONFIG_FAVORITES="${SCRIPT_DIR}/../roles/settings/files/favorites.conf"
 CONFIG_MUTTER="${SCRIPT_DIR}/../roles/settings/files/mutter.conf"
 CONFIG_INTERFACE="${SCRIPT_DIR}/../roles/settings/files/tweaks.conf"
 
 get_config() {
     dconf dump /org/gnome/desktop/app-folders/ >"${CONFIG_APP_FOLDERS}"
-    dconf dump /org/gnome/shell/ >"${CONFIG_FAVORITES}"
     dconf dump /org/gnome/mutter/ >"${CONFIG_MUTTER}"
     dconf dump /org/gnome/desktop/interface/ >"${CONFIG_INTERFACE}"
+    dconf read /org/gnome/shell/app-picker-layout >"${CONFIG_APP_PICKER}"
+    dconf read /org/gnome/shell/favorite-apps >"${CONFIG_FAVORITES}"
 }
 
 main() {
