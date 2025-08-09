62d0a58c64600a3802f781ebd61c8c8979cdfc4e
Bhavyai Gupta <zbhavyai@gmail.com>
2025-08-08 20:14:12 -0600

    add function to forward port from cmd line

--- roles/server/files/scripts/forward_ports.sh
+++ roles/server/files/scripts/forward_ports.sh
@@ -25,8 +25,9 @@ function Help() {
     echo "    ${0} [OPTION]"
     echo
     echo "Options:"
-    echo "    -f    forward the ports"
-    echo "    -h    show this help message"
+    echo "    -f        forward the ports defined in the script (see below)"
+    echo "    -p PORT   forward a port supplied from the command line"
+    echo "    -h        show this help message"
     echo
     echo "List of ports being temporarily forward in the firewall by this script: ${portsToForward[@]}"
     echo
@@ -35,6 +36,8 @@ function Help() {
     echo "-> Forward all the above ports"
     echo "    ${0} -f"
     echo
+    echo "-> Forward a port supplied from the command line"
+    echo "    ${0} -p 3306"
 }
 
 # prettyLog
@@ -47,7 +50,7 @@ function prettyLog() {
     printf "%s [%5s] %s.\n" "${TIMESTAMP}" "${LEVEL}" "${MESSAGE}"
 }
 
-# forward ports
+# forward ports that are predefined in the script
 # -------------------------------------------------------------------------------------
 function forwardPorts() {
     commandString="firewall-cmd --quiet"
@@ -65,9 +68,35 @@ function forwardPorts() {
     prettyLog "INFO" "Success"
 }
 
+# forward a port supplied from the command line
+# -------------------------------------------------------------------------------------
+function forwardPort() {
+    port=$1
+    if [[ -z "$port" ]]; then
+        prettyLog "ERROR" "No port supplied"
+        return 1
+    fi
+
+    if ! [[ "$port" =~ ^[0-9]+$ ]] || ((port < 1 || port > 65535)); then
+        prettyLog "ERROR" "Invalid port number: $port"
+        return 1
+    fi
+
+    commandString="firewall-cmd --quiet --add-port=${port}/tcp"
+
+    # execute the command
+    eval "${commandString}"
+
+    # don't persist the changes
+    # firewall-cmd --quiet --runtime-to-permanent
+    # firewall-cmd --quiet --reload
+
+    prettyLog "INFO" "Port ${port} forwarded successfully"
+}
+
 # driver code
 # -------------------------------------------------------------------------------------
-while getopts ":hf" opt; do
+while getopts ":hfp:" opt; do
     case "$opt" in
     h)
         Help
@@ -75,6 +104,15 @@ while getopts ":hf" opt; do
         ;;
     f)
         forwardPorts
+        exit
+        ;;
+    p)
+        if [[ -z "$OPTARG" ]]; then
+            prettyLog "ERROR" "No port supplied"
+            Help
+            exit
+        fi
+        forwardPort "$OPTARG"
         ;;
     \?)
         prettyLog "ERROR" "Invalid option"
