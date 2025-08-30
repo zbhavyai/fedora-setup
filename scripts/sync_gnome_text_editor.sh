26fbada77a7e9b8afaab46a689f264762f2bc086
Bhavyai Gupta <zbhavyai@gmail.com>
2025-08-30 12:37:31 -0600

    remove last-save-directory from text editor

--- scripts/sync_gnome_text_editor.sh
+++ scripts/sync_gnome_text_editor.sh
@@ -6,6 +6,7 @@ CONFIG_PREF="${SCRIPT_DIR}/../roles/gnome_text_editor/files/preferences.conf"
 
 get_config() {
     dconf dump /org/gnome/TextEditor/ >"${CONFIG_PREF}"
+    sed -i '/last-save-directory/d' "${CONFIG_PREF}"
 }
 
 main() {
