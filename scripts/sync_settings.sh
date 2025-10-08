0ebb791d272e6d7b14abf90f63413a3db97117cb
Bhavyai Gupta <zbhavyai@gmail.com>
2025-10-07 21:10:48 -0600

    split settings task into multiple files

--- scripts/sync_settings.sh
+++ scripts/sync_settings.sh
@@ -6,7 +6,7 @@ CONFIG_APP_FOLDERS="${SCRIPT_DIR}/../roles/settings/files/appFolders.conf"
 CONFIG_APP_PICKER="${SCRIPT_DIR}/../roles/settings/files/appPicker.conf"
 CONFIG_FAVORITES="${SCRIPT_DIR}/../roles/settings/files/favorites.conf"
 CONFIG_MUTTER="${SCRIPT_DIR}/../roles/settings/files/mutter.conf"
-CONFIG_INTERFACE="${SCRIPT_DIR}/../roles/settings/files/tweaks.conf"
+CONFIG_INTERFACE="${SCRIPT_DIR}/../roles/settings/files/desktop_interface.conf"
 
 function get_config() {
     dconf dump /org/gnome/desktop/app-folders/ >"${CONFIG_APP_FOLDERS}"
