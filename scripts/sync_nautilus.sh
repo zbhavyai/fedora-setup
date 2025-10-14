4e838b9f8994c706c037df68ded1b06d8ead5fa4
Bhavyai Gupta <zbhavyai@gmail.com>
2025-10-14 14:30:18 -0600

    add more nautilus settings

--- scripts/sync_nautilus.sh
+++ scripts/sync_nautilus.sh
@@ -3,9 +3,11 @@ set -euo pipefail
 
 SCRIPT_DIR="$(dirname "$0")"
 CONFIG_PREF="${SCRIPT_DIR}/../roles/nautilus/files/preferences.conf"
+CONFIG_FILECHOOSER="${SCRIPT_DIR}/../roles/nautilus/files/filechooser.conf"
 
 function get_config() {
     dconf dump /org/gnome/nautilus/preferences/ >"${CONFIG_PREF}"
+    dconf dump /org/gtk/gtk4/settings/file-chooser/ >"${CONFIG_FILECHOOSER}"
 }
 
 function main() {
